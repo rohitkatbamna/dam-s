2af52408e5aa5b2a94181d8d108ea48eb3be2982
rohit katbamna <rohitkatbamna@protonmail.com>
2022-03-12 09:55:41 +0530

    12TH MARCH 2022 09:55 AM COMMIT 1ST

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import logo from "../public/Assets/Logo.svg";
 import imageone from "../public/Assets/Hero Image.jpg";
+import checkboximage from "../public/Assets/Checkmark.svg";
+import airbnbimage from "../public/Assets/Airbnb.svg";
+import bookingimage from "../public/Assets/Booking.svg";
+import plumguide from "../public/Assets/Plum Guide.svg";
+
 export default function Home() {
 	return (
 		<div className={styles.container}>
@@ -22,9 +27,31 @@ export default function Home() {
 						camp on Earth.
 					</h1>
 					<p className={styles.text_subtitle}>
-						lorem Ipsum has been the industry's standard dummy text ever since
-						the 1500s, when an unknown printer
+						lorem Ipsum has been the industry&apos;s standard dummy text ever
+						since the 1500s, when an unknown printer
 					</p>
+					<div className={styles.check_box}>
+						<div className={styles.check_box_line}>
+							<Image src={checkboximage} alt="Checkmark" layout="fixed" />
+							<p className={styles.check_box_text}>Make up your own reviews</p>
+						</div>
+						<div className={styles.check_box_line}>
+							<Image src={checkboximage} alt="Checkmark" layout="fixed" />
+							<p className={styles.check_box_text}>Make up your own reviews</p>
+						</div>
+						<div className={styles.check_box_line}>
+							<Image src={checkboximage} alt="Checkmark" layout="fixed" />
+							<p className={styles.check_box_text}>Make up your own reviews</p>
+						</div>
+					</div>
+					<div className={styles.pat_with_part}>
+						<h6>Partnered With:</h6>
+						<div>
+							<Image src={airbnbimage} layout="fixed" alt="airbnb" />
+							<Image src={bookingimage} layout="fixed" alt="booking.com" />
+							<Image src={plumguide} layout="fixed" alt="plum guide" />
+						</div>
+					</div>
 				</main>
 			</body>
 			<Image src={imageone} alt="Main Image" />
